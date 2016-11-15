986101fc92bfbc0de53e942cd6fa612318ba7f33
normoneil <normoneil@users.noreply.github.com>
2016-11-14 20:44:37 -0600

    Should be good now

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,4 +1,5 @@
 class Actor < ActiveRecord::Base
   # - name: must be present; must be unique in combination with dob
   validates :name, :presence => true, :uniqueness => { :scope => :dob }
+  has_many(:characters, :class_name => "Character", :foreign_key => "actor_id")
 end
--- app/models/director.rb
+++ app/models/director.rb
@@ -1,5 +1,7 @@
 class Director < ActiveRecord::Base
  # - name: must be present; must be unique in combination with dob
  validates :name, :presence => true, :uniqueness => { :scope => :dob }
+
+ belongs_to(:movie, :class_name => "Movie", :foreign_key => "director_id")
  has_many(:movies, :class_name => "Movie", :foreign_key => "director_id")
 end
