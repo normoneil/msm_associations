70cb5a649691790a026da0b67aa51de974052019
normoneil <normoneil@users.noreply.github.com>
2016-11-14 19:34:35 -0600

    about to make the changes

--- app/models/director.rb
+++ app/models/director.rb
@@ -1,4 +1,5 @@
 class Director < ActiveRecord::Base
  # - name: must be present; must be unique in combination with dob
  validates :name, :presence => true, :uniqueness => { :scope => :dob }
+ has_many(:movies, :class_name => "Movie", :foreign_key => "director_id")
 end
